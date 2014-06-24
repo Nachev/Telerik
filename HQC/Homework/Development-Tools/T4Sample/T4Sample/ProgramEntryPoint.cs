6780285e05e56641ffdae86a49a38a9dccc3c177
Nachev <n.n.nachev@gmail.com>
2014-06-24 18:39:11 +0300

    Changed Picture Git. Added GitHub repository link in file .txt and T4 Sample solution.

---
+++ HQC/Homework/Development-Tools/T4Sample/T4Sample/ProgramEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿namespace T4Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ProgramEntryPoint
+    {
+        public static void Main()
+        {
+            T4SampleClass.HelloTFour();
+        }
+    }
+}
