5a80cfef21d5bab9ca069e56101a8ebd8d4789d3
Nachev <n.n.nachev@gmail.com>
2014-08-27 18:51:47 +0300

    Added homeworks for ADO.NET

---
+++ Databases/Homeworks/ADO.NET/GetNameAndDescriptionOfCategories/GetNameAndDescription.cs
@@ -0,0 +1,29 @@
+﻿namespace GetNameAndDescriptionOfCategories
+{
+    using System;
+    using System.Data.SqlClient;
+
+    /*Write a program that retrieves the name and 
+    description of all categories in the NorthwindDB.*/
+    public class GetNameAndDescription
+    {
+        private static void Main()
+        {
+            SqlConnection con = new SqlConnection(
+                "Server=.;Database=Northwind; Integrated Security=true");
+            con.Open();
+            using (con)
+            {
+                SqlCommand cmdCount = new SqlCommand(
+                    "SELECT CategoryName, Description FROM Categories", con);
+                using (var nameAndDescriptionReader = cmdCount.ExecuteReader())
+                {
+                    while (nameAndDescriptionReader.Read())
+                    {
+                        Console.WriteLine("Category name: {0}, description: {1},", nameAndDescriptionReader.GetValue(0), nameAndDescriptionReader.GetValue(1));
+                    }
+                }
+            }
+        }
+    }
+}
